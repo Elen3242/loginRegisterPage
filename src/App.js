2517cbd5c08c71d5d0d982813f2b53eb337e9915
Elen3242 <elenh9849@gmail.com>
2023-07-23 18:22:30 +0400

    create login and register page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './view/pages/login';
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import Register from './view/pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter> 
+          <Routes>
+             <Route path='/login' element={<Login />} />
+             <Route path='/register' element={<Register />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/view/pages/register/index.js
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import { 
+    Box, 
+    Container, 
+    Typography, 
+    TextField, 
+    Button 
+ } from '@mui/material';
+
+const Register = () => {
+    const [registerValue, setRegisterValue] = useState({
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+    console.log(registerValue);
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setRegisterValue({
+            ...registerValue,
+            [name]: value
+        });
+    }
+    const handleRegister = () => {
+        console.log(registerValue);
+        // Here, you can implement the logic for submitting the registration data to your server/database
+    }
+    return (
+        <Container maxWidth='xs'>
+            <Box sx={{
+                  margin: '20px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+            }}>
+                <Typography 
+                variant="h5"
+                sx={{
+                    margin: '20px',
+                }}
+                >
+                    Register
+                </Typography>
+                <Box component='form'
+                    sx={{
+                        display: 'grid',
+                        gap: '20px'
+                    }}
+                >
+
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='firstName'
+                            type='text' 
+                            label='First Name' 
+                            placeholder='First Name' 
+                        />
+                    </div>
+
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='lastName'
+                            type='text' 
+                            label='Last Name' 
+                            placeholder='Last Name' 
+                        />
+                    </div>
+
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='age'
+                            type='number' 
+                            label='age' 
+                            placeholder='Age' 
+                        />
+                    </div>
+
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='phoneNumber'
+                            type='text' 
+                            label='Phone number' 
+                            placeholder='Phone number' 
+                        />
+                    </div>
+
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='email'
+                            type='email' 
+                            label='Email' 
+                            placeholder='Email' 
+                        />
+                    </div>
+                    
+                    <div>
+                        <TextField 
+                            fullWidth
+                            onChange={handleInputChange}
+                            name='password'
+                            type='password' 
+                            label='Password'
+                            placeholder='Password'
+                        />
+                    </div>
+                    
+                    <div>
+                        <Button variant='contained' onClick={handleRegister}>
+                            Register
+                        </Button>
+                    </div>
+                </Box>
+            </Box>
+        </Container>
+    )
+};
+
+export default Register;
+
+
+
+
+
+
