b81cf661eb56986f753e67a3f0801bb997fe86ad
Elen3242 <elenh9849@gmail.com>
2023-07-26 21:39:44 +0400

    add register button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
+import {BrowserRouter , Routes, Route } from 'react-router-dom';
 import Login from './view/pages/login';
-import { BrowserRouter , Route , Routes } from 'react-router-dom'
 import Register from './view/pages/register';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <BrowserRouter> 
-          <Routes>
-             <Route path='/login' element={<Login />} />
-             <Route path='/register' element={<Register />} />
-          </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+    {/* Add more routes if needed */}
+    </BrowserRouter>
   );
-}
-
+};
 
 export default App;
